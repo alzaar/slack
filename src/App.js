164cd2f300e363ccb75d8f5c6a20b9be2964e399
ali zahir <alizahir@live.com>
2018-11-17 00:06:14 -0500

    working on this

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react'
-
+import UsernameForm from './components/UsernameForm';
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentUsername: '',
+
+    }
+    this.onUsernameSubmitted = this.onUsernameSubmitted.bind(this);
+  }
+
+  onUsernameSubmitted(username) {
+    fetch('/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+
+      },
+      body: JSON.stringify({username}),
+    })
+    .then(res => {
+      this.setState({currentUsername: username})
+    })
+    .catch(error => console.log(error))
+  }
   render() {
-    return <h1>Chatly</h1>
+    return (
+      <div>
+        <UsernameForm onSubmit={this.onUsernameSubmitted} />
+      </div>
+  );
   }
 }
 
---
+++ src/components/UsernameForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class UsernameForm extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: '',
+    }
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    this.props.onSubmit(this.state.username);
+  }
+
+  onChange(e) {
+    this.setState({username: e.target.value})
+  }
+  render() {
+    return (
+        <div>
+          <h2>What is your username?</h2>
+          <form onSubmit={this.onSubmit} >
+            <input type="text" placeholder="Please enter!" onChnage={this.onChange} />
+            <input type="submit" />
+          </form>
+        </div>
+    );
+  }
+}
+
+export default UsernameForm;
